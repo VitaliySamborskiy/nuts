4815f502fc7b6e1c26a67c2299fd231fb7673aeb
Vitaliy <vitaliysamborsky2002@gmail.com>
2024-11-18 18:14:30 +0200

    fix bugs

--- src/js/get-images.js
+++ src/js/get-images.js
@@ -4,6 +4,7 @@ import { getElement } from "./base/get-methods/get-element-dom.js";
 export function getImages() {
     useGetImg("logo-1.svg", getElement(".header__center-logo"));
     useGetImg("logo-1.svg", getElement(".burger__logo-img"));
+    useGetImg("logo-1.svg", getElement(".footer__logo-img"));
     useGetImg("user.svg", getElement(".header__top-img"));
     useGetImg("user.svg", getElement(".burger__img"));
     useGetImg("gs://nuts-17b69.appspot.com/31318052.webp", getElement(".error__img"));
